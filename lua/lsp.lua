6a4536b3dee4c16dfd0a5162ac69bc09229ca148
PaltosLP <motata.frochaux@gmail.com>
2022-04-05 16:04:50 +0200

    diagnostics

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -69,7 +69,14 @@ local cmp = require'cmp'
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
   require('lspconfig')['sumneko_lua'].setup {
-    capabilities = capabilities
+    capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { 'vim' }
+			}
+		}
+	}
   }
   require('lspconfig')['pyright'].setup {
     capabilities = capabilities
