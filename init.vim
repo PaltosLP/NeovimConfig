d697c9041c9e3dfc9728e8b8930719c400109a7b
PaltosLP <motata.frochaux@gmail.com>
2022-04-06 11:57:18 +0200

    maps

--- init.vim
+++ init.vim
@@ -13,16 +13,16 @@ set smartindent
 
 
 "mappings
-let mapleader = " "
-inoremap jj <ESC>
-nnoremap <C-t> :NERDTreeToggle<CR>
-nnoremap <C-a> :call run_file#Run_File()<Cr>
-nnoremap <C-s> :call save#w()<Cr>
-nnoremap <C-r> :call number_set#relative_numb()<Cr>
-nnoremap <C-n> :call number_set#normal_numb()<Cr>
-nnoremap <leader>ff :Telescope find_files<cr>
+" let mapleader = " "
+"inoremap jj <ESC>
+"nnoremap <C-t> :NERDTreeToggle<CR>
+" nnoremap <C-a> :call run_file#Run_File()<Cr>
+" nnoremap <C-s> :call save#w()<Cr>
+" nnoremap <C-r> :call number_set#relative_numb()<Cr>
+" nnoremap <C-n> :call number_set#normal_numb()<Cr>
+" nnoremap <leader>ff :Telescope find_files<cr>
 
-"lua require 'mappings'
+lua require 'mappings'
 
 
 "colors
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,6 +4,48 @@ vim.g.mapleader = ' '
 vim.api.nvim_set_keymap(
   'n',
   '<leader>ff',
-  ':Telescope find_files()<cr>',
-  { noremap = true }
+  ':Telescope find_files<Cr>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-t>',
+  ':NERDTreeToggle<Cr>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'i',
+  'jj',
+  '<ESC>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-a>',
+  ':call run_file#Run_File()<Cr>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-s>',
+  ':call save#w()<Cr>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-r>',
+  ':call number_set#relative_numb()<Cr>',
+  { noremap = true, silent = true }
+ )
+
+vim.api.nvim_set_keymap(
+  'n',
+  '<C-n>',
+  ':call number_set#normal_numb()<Cr>',
+  { noremap = true, silent = true }
  )
