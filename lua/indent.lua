f3721e266c152fbcf7f07485a3f70f6dc01c3305
PaltosLP <PaltosLP@github.com>
2022-04-13 11:50:15 +0200

    go indent

--- lua/indent.lua
+++ lua/indent.lua
@@ -3,8 +3,12 @@
 
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
-    show_current_context = true,
+    --show_current_context = true,
     -- show_current_context_start = true,
+
+	char = "▏",
+    use_treesitter = true,
+    --show_first_indent_level = false,
 	filetype_exclude = {
 		'help',
 		'dashboard',
