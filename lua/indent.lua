a81b38fa5fea211c13da095b2312f8062c002564
PaltosLP <PaltosLP@github.com>
2022-04-13 11:46:05 +0200

    no dashboard idnent

--- lua/indent.lua
+++ lua/indent.lua
@@ -5,4 +5,15 @@ require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
     show_current_context = true,
     -- show_current_context_start = true,
+	filetype_exclude = {
+		'help',
+		'dashboard',
+		'git',
+		'markdown',
+		'text',
+		'terminal',
+		'lspinfo',
+		'packer',
+		'NvimTree',
+  }
 }
