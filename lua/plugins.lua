618cf84e0c242da17a29bb99e6d8d1def150abe0
PaltosLP <PaltosLP@github.com>
2022-04-16 17:10:23 +0200

    pack

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,7 @@ vim.cmd([[
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
