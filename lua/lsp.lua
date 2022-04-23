bd93ee43c15e54ddb021f37cee3057106ce15de2
PaltosLP <motata.frochaux@gmail.com>
2022-04-23 19:24:19 +0200

    lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -83,12 +83,14 @@ cmp.setup({
 	-- Setup lspconfig.
 	local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 	-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-	require('lspconfig')['sumneko_lua'].setup {
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { 'vim' }
+
+
+require('lspconfig')['sumneko_lua'].setup {
+capabilities = capabilities,
+settings = {
+	Lua = {
+		diagnostics = {
+			globals = { 'vim' }
 			}
 		}
 	}
