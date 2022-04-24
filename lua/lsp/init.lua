468a79a0d5fba9a977703f046028f7fdbf5fa695
PaltosLP <motata.frochaux@gmail.com>
2022-04-24 16:48:43 +0200

    hope

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -4,6 +4,27 @@ require 'lsp.comp'
 
 local ft = vim.fn.expand('%:e')
 
+local possibles = { 'py', 'lua', 'vim', 'c', 'cpp' }
+local state = false
+
+
+for key, value in pairs(possibles) do
+	-- local occur = false
+	print( value, ft )
+	print(type(value), type(ft))
+	if value == 'ft' then
+		state = true
+		print('worked')
+	end
+end
+
+if state then
+	print('worked')
+	ft = ft .. 'lsp'
+	local ft_extended = 'lsp.settings.' .. ft
+	require(ft_extended)
+end
+
 -- if ft in ['py', 'lua', 'c'] then
 -- blablabla
 -- else
@@ -11,10 +32,10 @@ local ft = vim.fn.expand('%:e')
 
 
 -- print('ft =', ft)
-ft = ft .. 'lsp'
+-- ft = ft .. 'lsp'
 -- print('filetype =', ft)
 
-local ft_extended = 'lsp.settings.' .. ft
+-- local ft_extended = 'lsp.settings.' .. ft
 -- print('blub', ft2) --prints: lsp.pylsp
 
-require(ft_extended)
+-- require(ft_extended)
