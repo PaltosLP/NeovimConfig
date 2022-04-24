9c8d120d8d25ac829562b36a7d2f0f9bd92efb03
PaltosLP <motata.frochaux@gmail.com>
2022-04-24 16:05:18 +0200

    go

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -3,6 +3,24 @@
 require 'lsp.comp'
 
 local ft = vim.fn.expand('%:e')
+
+-- if ft == 'dashboard' then
+-- 	return 0
+-- elseif ft == 'netrw' then
+-- 	return 0
+--
+-- else
+-- 	ft = ft .. 'lsp'
+-- 	-- print('filetype =', ft)
+--
+-- 	local ft_extended = 'lsp.' .. ft
+-- 	-- print('blub', ft2) --prints: lsp.pylsp
+--
+-- 	require(ft_extended)
+-- end
+
+print('ft =', ft)
+
 ft = ft .. 'lsp'
 -- print('filetype =', ft)
 
