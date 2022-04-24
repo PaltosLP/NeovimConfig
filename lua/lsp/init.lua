90ce554555888caa3e09a1d0f78b3567cf64a295
PaltosLP <motata.frochaux@gmail.com>
2022-04-24 23:01:00 +0200

    damn

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,11 +1,7 @@
-
-
--- local opts = { noremap=true, silent=true }
 -- vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 -- vim.api.nvim_set_keymap('n', '<leader>d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 -- vim.api.nvim_set_keymap('n', '<leader>f', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 -- vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-
 
 
 require 'lsp.comp'
@@ -16,7 +12,7 @@ local possibles = { 'py', 'lua', 'vim', 'c', 'cpp' }
 local state = false
 
 
-for key, value in pairs(possibles) do
+for _, value in pairs(possibles) do
 	-- local occur = false
 	if value == ft then
 		state = true
@@ -28,18 +24,3 @@ if state then
 	local ft_extended = 'lsp.langs.' .. ft .. 'lsp'
 	require(ft_extended)
 end
-
--- if ft in ['py', 'lua', 'c'] then
--- blablabla
--- else
--- pass
-
-
--- print('ft =', ft)
--- ft = ft .. 'lsp'
--- print('filetype =', ft)
-
--- local ft_extended = 'lsp.settings.' .. ft
--- print('blub', ft2) --prints: lsp.pylsp
-
--- require(ft_extended)
--- lua/lsp/langs/cpplsp.lua
+++ lua/lsp/langs/cpplsp.lua
@@ -1,7 +1,25 @@
 
-
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.  make_client_capabilities())
 
+local opts = { noremap=true, silent=true }
+local on_attach = function(client, bufnr)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+   -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+end
+
+
+
 require('lspconfig')['clangd'].setup {
+	on_attach = on_attach,
 	capabilities = capabilities
 }
