87e183a7f2f90db99a1dd094ccdae131c85ebc81
PaltosLP <motata.frochaux@gmail.com>
2022-04-29 11:27:35 +0200

    cmp

--- lua/lsp/comp.lua
+++ lua/lsp/comp.lua
@@ -12,111 +12,123 @@ for sign, icon in pairs(signs) do
 	)
 end
 
+local check_backspace = function()
+  local col = vim.fn.col "." - 1
+  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
+end
 
+
+local kind_icons = {
+	Text = "",
+	Method = "",
+	Function = "",
+	-- Function = "GG",
+	Constructor = "",
+	Field = "ﰠ",
+	Variable = "",
+	Class = "ﴯ",
+	Interface = "",
+	Module = "",
+	Property = "ﰠ",
+	Unit = "塞",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "פּ",
+	Event = "",
+	Operator = "",
+	TypeParameter = ""
+	}
+-- find more here: https://www.nerdfonts.com/cheat-sheet
 cmp.setup({
 	snippet = {
-	  -- REQUIRED - you must specify a snippet engine
 	  expand = function(args)
-		-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 		require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-		-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-		-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 	  end,
 		},
 	window = {
 	  -- completion = cmp.config.window.bordered(),
 	  -- documentation = cmp.config.window.bordered(),
+		documentation = {
+   			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  		},
 		},
-	-- mapping = cmp.mapping.preset.insert({
-	--   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-	--   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-	--   ['<C-o>'] = cmp.mapping.complete(),
-	--   ['<C-e>'] = cmp.mapping.abort(),
-	--   ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	-- 	}),
 	 mapping = {
-        ["<cr>"] = cmp.mapping.confirm(),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+	    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+		["<C-e>"] = cmp.mapping {
+     		i = cmp.mapping.abort(),
+      		c = cmp.mapping.close(),
+			},
+        ["<cr>"] = cmp.mapping.confirm { select = true },
+		["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expandable() then
+					luasnip.expand()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif check_backspace() then
+					fallback()
+				else
+					fallback()
+				end
+			end, { "i", "s" }
+			),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s"	}
+			),
+	},
 
-        ["<S-q>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-        },
 
 
 	sources = cmp.config.sources({
-	  { name = 'nvim_lsp' },
-	  -- { name = 'vsnip' }, -- For vsnip users.
-	  { name = 'luasnip' }, -- For luasnip users.
-	  -- { name = 'ultisnips' }, -- For ultisnips users.
-	  -- { name = 'snippy' }, -- For snippy users.
-	  { name = 'look' },
-	  { name = 'buffer' },
-		},
-	{
-	  { name = 'buffer' },
-	}),
+			-- { name = 'nvim_lsp' },
+			{ name = 'luasnip' }, -- For luasnip users.
+			-- { name = 'look' },
+			{ name = 'buffer' },
+			{ name = 'path' },
+		}
+	),
 	formatting = {
 		fields = { cmp.ItemField.Kind, cmp.ItemField.Abbr, cmp.ItemField.Menu },
-		format = lspkind.cmp_format({ with_text = false }),
-		symbol_map = {
-		  Text = "",
-		  Method = "",
-		  -- Function = "",
-		  Function = "GG",
-		  Constructor = "",
-		  Field = "ﰠ",
-		  Variable = "",
-		  Class = "ﴯ",
-		  Interface = "",
-		  Module = "",
-		  Property = "ﰠ",
-		  Unit = "塞",
-		  Value = "",
-		  Enum = "",
-		  Keyword = "",
-		  Snippet = "",
-		  Color = "",
-		  File = "",
-		  Reference = "",
-		  Folder = "",
-		  EnumMember = "",
-		  Constant = "",
-		  Struct = "פּ",
-		  Event = "",
-		  Operator = "",
-		  TypeParameter = ""
-		},
-    -- format = lspkind.cmp_format({
-    --   mode = 'symbol', -- show only symbol annotations
-    --   maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-    --
-    --   -- The function below will be called before any actual modifications from lspkind
-    --   -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-    --   -- before = function (entry, vim_item)
-    --   --   ...
-    --   --   return vim_item
-    --   -- end
-    -- })
-  }
+		format = function(entry, vim_item)
+		-- Kind icons
+		vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+		-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+		vim_item.menu = ({
+			luasnip = "[Snippet]",
+			buffer = "[Buffer]",
+			path = "[Path]",
+		})[entry.source.name]
+			return vim_item
+		end,
+		maxwidth = 50,
+  			},
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+ 		},
+  	experimental = {
+		ghost_text = false,
+		native_menu = false,
+  		},
 })
 
 	-- Set configuration for specific filetype.
@@ -147,8 +159,3 @@ cmp.setup.cmdline(':', {
 	  { name = 'cmdline' }
 	})
 })
-
--- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-
