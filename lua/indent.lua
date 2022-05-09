d3c8de9f9769aee3112c92fc54d13cf6b0695284
PaltosLP <motata.frochaux@gmail.com>
2022-05-09 11:53:42 +0200

    nice

--- lua/indent.lua
+++ lua/indent.lua
@@ -16,7 +16,8 @@ require("indent_blankline").setup {
     --show_current_context = true,
     -- show_current_context_start = true,
 
-	char = "▏",
+	-- char = "▏",
+	char = "︱",
     use_treesitter = true,
     --show_first_indent_level = false,
 	space_char_blankline = " ",
