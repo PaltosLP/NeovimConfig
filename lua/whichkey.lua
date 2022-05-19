c97b81f20e711b03d68d00c5b6ccfdc87dc80688
PaltosLP <motata.frochaux@gmail.com>
2022-05-19 10:34:16 +0200

    go

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -87,12 +87,10 @@ vim.g.mapleader = ' '
 wk.register({
   f = {
     name = "file", -- optional group name
-    f = { "<cmd>Telescope find_files<cr>", "Find File" }, -- create a binding with label
-    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File", noremap=false }, -- additional options for creating the keymap
-    n = { "New File" }, -- just a label. don't create any mapping
-    e = "Edit File", -- same as above
-    ["1"] = "which_key_ignore",  -- special label to hide it in the popup
-    b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
+    f = { "<cmd>Telescope find_files<cr>", "Find File" },
+    g = { "<cmd>Telescope live_grep<cr>", "Search Text" },
+	b = { "<cmd>Telescope buffers<Cr>", "Search Buffer" },
+	h = { "<cmd>Telescope help_tags<Cr>", "Search Help Tags" },
   },
 }, { prefix = "<leader>" })
 
