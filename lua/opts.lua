ffe7c77f5232ea25e792a698b6126d852976fa1d
PaltosLP <motata.frochaux@gmail.com>
2022-05-25 08:52:24 +0200

    🕜timeoulen

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,6 +1,4 @@
-
-
-
+-- Options
 vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
@@ -9,7 +7,7 @@ vim.opt.scrolloff = 8
 vim.o.encoding = "utf-8"
 vim.o.backspace = "indent,eol,start"
 vim.o.completeopt = 'menu,menuone,noselect'
--- vim.opt.smartindent = true
+vim.opt.smartindent = true
 vim.opt.termguicolors = true
 vim.o.sidescrolloff = 5
 vim.cmd 'filetype plugin indent on'
@@ -20,7 +18,7 @@ vim.opt.cmdheight = 2
 vim.opt.swapfile = false
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
-
+vim.o.timeoutlen = 600
 
 -- To make the Boxes nicer
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {	border = "rounded" })
