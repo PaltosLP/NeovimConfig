782412b758d127a524cb3a59a61d8d5ad7862a09
PaltosLP <motata.frochaux@gmail.com>
2022-05-27 22:26:19 +0200

    hex colors

--- lua/highlights/tree-sitter.lua
+++ lua/highlights/tree-sitter.lua
@@ -32,7 +32,13 @@ configs.setup {
     -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
     extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
+ --    colors = {
+	-- 	"#FF0000", --pink
+	-- 	"#FF00FF", --yellow
+	-- 	"#FFFF00", --purple
+	-- 	"#6600FF",
+	-- 	"#0000FF",
+	-- }, -- table of hex strings
     -- termcolors = {} -- table of colour name strings
   },
   autotag = {
