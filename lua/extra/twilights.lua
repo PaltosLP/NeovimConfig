726bd64426bebeee7f1f339a7b2ee97a35b69e40
PaltosLP <motata.frochaux@gmail.com>
2022-06-06 13:09:05 +0200

    gitsigns

--- lua/extra/twilights.lua
+++ lua/extra/twilights.lua
@@ -3,7 +3,7 @@ require'twilight'.setup{
   dimming = {
     alpha = 0.25, -- amount of dimming
     -- we try to get the foreground from the highlight groups or fallback color
-    color = { "Normal", "#ffffff" },
+    color = { "Normal", "#568E61" },
     inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
   },
   context = 10, -- amount of lines we will try to show around the current line
