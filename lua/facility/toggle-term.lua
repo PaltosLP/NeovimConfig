d59b12adb0819c3a362cc0dff4d013697f02349b
PaltosLP <motata.frochaux@gmail.com>
2022-06-18 14:51:12 +0200

    nice

--- lua/facility/toggle-term.lua
+++ lua/facility/toggle-term.lua
@@ -3,7 +3,21 @@ require("toggleterm").setup{
 
 	size = 10,
 	open_mapping = [[<c-e>]],
+	hide_numbers = true,
 	terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
-
+	start_in_insert=true,
+	insert_mappings=true,
+	direction = "horizontal",
+	-- direction="float",
+	-- float_opts={
+	-- 	border="curved",
+	-- 	width = 100,
+	--     height = 10,
+	-- 	winblend=0,
+	-- 	highlights={
+	-- 		border="Normal",
+	-- 		background="Normal",
+	-- 	},
+	-- },
 
 }
