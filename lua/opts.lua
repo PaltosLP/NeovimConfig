827e54ee1ea7bcfdb93cab187d8445c940d73cef
PaltosLP <motata.frochaux@gmail.com>
2022-06-27 14:28:57 +0200

    disable builtins

--- lua/opts.lua
+++ lua/opts.lua
@@ -35,3 +35,31 @@ vim.cmd 'set wildignore+=blue.vim,darkblue.vim,default.vim,delek.vim,desert.vim,
 
 -- set pumblend=15
 -- hi PmenuSel blend=0
+
+
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    vim.g["loaded_" .. plugin] = 1
+end
+
+
