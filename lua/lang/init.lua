31ec748ba4f5543b922d9bd36c9d958e9fe3f71c
PaltosLP <motata.frochaux@gmail.com>
2022-07-02 15:57:08 +0200

    yeyeyeyeye

--- lua/lang/init.lua
+++ lua/lang/init.lua
@@ -1,7 +1,4 @@
 
-
-
-
 local event = { 'InsertEnter', 'CmdlineEnter' }
 
 vim.api.nvim_create_autocmd(event, {
@@ -13,15 +10,47 @@ vim.api.nvim_create_autocmd(event, {
 		-- 	PackerLoad nvim-cmp
 		-- 	PackerLoad cmp-nvim-lsp
 		-- ]])
-		-- print("fdsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-		-- require 'lang.choice'
 		require 'lang.comp'
 		require 'lang.langs.snips'
 		require 'lang.snippet'
 		end
 })
 
-require 'lang.choice'
+
+-- vim.wo.foldcolumn = '1'
+-- vim.wo.foldlevel = 99 -- feel free to decrease the value
+-- vim.wo.foldenable = true
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true
+}
+
+local servers = { 'sumneko_lua', 'clangd', 'gopls', 'quick_lint_js', 'pyright', 'vimls' }
+local language_servers = {}
+
+local ft = vim.fn.expand('%:e')
+if ft == '' then
+	ft = 'none'
+end
+
+local on_attach_path = 'lang.langs.' .. ft
+local config = { capabilities = capabilities, on_attach = require(on_attach_path), settings = { Lua = { diagnostics = { globals = { 'vim' } } } } }
 
 
+for _,server in pairs(servers) do
+	language_servers[server] = config
+end
+
+
+
+for ls,conf in pairs(language_servers) do
+    require('lspconfig')[ls].setup(
+	conf
+	)
+end
+
+
+-- require('ufo').setup()
 
--- lua/lang/langs/empty.lua
+++
@@ -1,9 +0,0 @@
-
-
-vim.cmd 'PackerLoad dashboard-nvim'
-
-require 'extra.dashboard'
-
-vim.cmd 'Dashboard'
-
-
