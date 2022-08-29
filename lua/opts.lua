900dcb6c804dd5bf529b506b67547649d938efa3
PaltosLP <motata.frochaux@gmail.com>
2022-08-29 20:06:28 +0200

    0 height yeye

--- lua/opts.lua
+++ lua/opts.lua
@@ -14,7 +14,7 @@ vim.o.sidescrolloff = 5
 vim.cmd 'filetype plugin indent on'
 vim.opt.backup = false
 -- vim.opt.clipboard = "unnamedplus"
-vim.opt.cmdheight = 2
+vim.opt.cmdheight = 0
 vim.opt.mouse = "a"
 vim.opt.swapfile = false
 vim.opt.signcolumn = "yes"
