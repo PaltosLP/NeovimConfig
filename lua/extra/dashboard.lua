2a177d5c878b5bbe13fa76595d32b0e605f31b2d
PaltosLP <motata.frochaux@gmail.com>
2022-09-19 09:18:53 +0200

    fix dashboard handbook

--- lua/extra/dashboard.lua
+++ lua/extra/dashboard.lua
@@ -32,7 +32,7 @@ db.custom_center = {
   { icon = ' ', desc = 'Find File', action = 'Telescope find_files'},
   { icon = ' ', desc = 'Search Text', action = 'Telescope live_grep'},
   { icon = ' ', desc = 'Recent Files', action = 'Telescope oldfiles'},
-  { icon = ' ', desc = 'Handbook', action = 'edit  ~/.config/nvim/handbook.txt'},
+  { icon = ' ', desc = 'Handbook', action = 'edit  ~/.config/nvim/handbook.md'},
   { icon = ' ', desc = 'Code', action = 'edit /home/paltos/files'},
   { icon = ' ', desc = 'Config', action = 'edit ~/.config/nvim/init.lua'}
 }
