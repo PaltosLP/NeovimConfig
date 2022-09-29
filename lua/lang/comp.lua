7b17204a7c2c971ce022f58b244dc141ed217377
PaltosLP <motata.frochaux@gmail.com>
2022-09-29 08:58:57 +0200

    cmp goes brrr

--- lua/lang/comp.lua
+++ lua/lang/comp.lua
@@ -32,32 +32,32 @@ end
 
 
 local kind_icons = {
-	Text = "",
-	Method = "",
-	Function = "",
+	Text = "  ",
+	Method = "  ",
+	Function = "  ",
 	-- Function = "GG",
-	Constructor = "",
-	Field = "ﰠ",
-	Variable = "",
-	Class = "ﴯ",
-	Interface = "",
-	Module = "",
-	Property = "ﰠ",
-	Unit = "塞",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "פּ",
-	Event = "",
-	Operator = "",
-	TypeParameter = ""
+	Constructor = "  ",
+	Field = " ﰠ ",
+	Variable = "  ",
+	Class = " ﴯ ",
+	Interface = "  ",
+	Module = "  ",
+	Property = " ﰠ ",
+	Unit = " 塞 ",
+	Value = "  ",
+	Enum = "  ",
+	Keyword = "  ",
+	Snippet = "  ",
+	Color = "  ",
+	File = "  ",
+	Reference = "  ",
+	Folder = "  ",
+	EnumMember = "  ",
+	Constant = "  ",
+	Struct = " פּ ",
+	Event = "  ",
+	Operator = "  ",
+	TypeParameter = "   "
 	}
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -74,7 +74,9 @@ cmp.setup({
 	  end,
 		},
 	window = {
-
+		winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+        col_offset = -3,
+        side_padding = 0,
 		-- completion = cmp.config.window.bordered(),
 		-- documentation = cmp.config.window.bordered(),
    		-- border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
@@ -187,7 +189,7 @@ cmp.setup.cmdline(':', {
 	})
 })
 
-
+--vscode like 
 vim.cmd([[ 
 " gray
 highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
@@ -207,4 +209,52 @@ highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
 highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
 ]])
 
+
+--menu
+local highl = {
+  PmenuSel = { bg = "#282C34", fg = "NONE" },
+  Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+
+  CmpItemAbbrDeprecated = { fg = "#7E8294", strikethrough=true},
+  CmpItemAbbrMatch = { fg = "#82AAFF", bold=true },
+  CmpItemAbbrMatchFuzzy = { fg = "#82AAFF",  bold=true },
+  CmpItemMenu = { fg = "#C792EA",italic=true },
+
+  CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+  CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+  CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+
+  CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+  CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+  CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+
+  CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+  CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+  CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+
+  CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+  CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+  CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+  CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+  CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+
+  CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+  CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+
+  CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+  CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+  CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+
+  CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+  CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+  CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+
+  CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+  CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+  CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+}
+
+for hl_name, hl_table in pairs(highl) do
+	vim.api.nvim_set_hl(0,hl_name,hl_table)
+end
 
