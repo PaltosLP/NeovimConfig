92722d5f5e4163a5b368b83b2c5aa532f9b45b62
PaltosLP <motata.frochaux@gmail.com>
2022-10-11 21:43:00 +0200

    wait its useless

--- lua/lang/langs/snips.lua
+++ lua/lang/langs/snips.lua
@@ -39,19 +39,6 @@ ls.add_snippets('c', {
 	})
 })
 
-ls.add_snippets('c', {
-	s('Alphabet_Low', {
-		t("char low_alpha[] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};")
-
-	})
-})
-
-ls.add_snippets('c', {
-	s('Alphabet_Upper', {
-		t("char upp_alpha[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};")
-
-	})
-})
 
 --------------------------------------------------------------------
 
