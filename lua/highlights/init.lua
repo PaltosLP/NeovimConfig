0523bfca934cae6689cdb8fadcc556fa8b9bd783
PaltosLP <motata.frochaux@gmail.com>
2022-10-24 09:16:50 +0200

    treesit problem

--- lua/highlights/init.lua
+++ lua/highlights/init.lua
@@ -38,7 +38,7 @@ vim.cmd[[highlight LineNr guifg=grey]]
 
 
 -- Highlight the @foo.bar capture group with the "Identifier" highlight group
-vim.api.nvim_set_hl(0, "@variable", { link = "TSField", bold = 1 })
+vim.api.nvim_set_hl(0, "@variable", { link = "rainbowcol4" })
 vim.api.nvim_set_hl(0, "@field", { link = "SpecialKey" })
 vim.api.nvim_set_hl(0, "@keyword.function", { link = "keyword" })
 
