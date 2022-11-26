c2c4016ff30a6fc1220669dd53240c50fa60c226
PaltosLP <motata.frochaux@gmail.com>
2022-11-26 14:23:59 +0100

    clean

--- init.lua
+++ init.lua
@@ -1,24 +1,24 @@
 --Personal Configs
-
 require 'impatient'.enable_profile()
 
-
-vim.g.minimal_config = false
-vim.g.user_interface = false
-
-
-
+-----------------------------
+--First Requirements
 require 'core.opts'
 require 'core.plugins'
 require 'core.mappings'
+-----------------------------
 
 
+
+-----------------------------
+--Customizations
 vim.cmd.colorscheme("dracula")
--- vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
--- vim.cmd[[highlight NormalFloat  guibg=none]]
--- vim.cmd[[highlight FloatBorder  guibg=none]]
--- vim.cmd[[highlight LineNr guifg=grey guibg=none]]
--- vim.cmd[[highlight SignColumn guibg=none]]
--- vim.api.nvim_set_hl(0, "CursorLineNr", { link = "@function.call" })
+vim.g.minimal_config = false
+vim.g.user_interface = false
+vim.g.transparent = false
+-----------------------------
 
+
+
+--further configs
 require 'charon'
--- lua/charon.lua
+++ lua/charon.lua
@@ -10,3 +10,12 @@ end
 if vim.g.user_interface then
 	require 'ui'
 end
+
+if vim.g.transparent then
+	vim.cmd[[hi Normal guibg=NONE ctermbg=NONE]]
+	vim.cmd[[highlight NormalFloat  guibg=none]]
+	vim.cmd[[highlight FloatBorder  guibg=none]]
+	vim.cmd[[highlight LineNr guifg=grey guibg=none]]
+	vim.cmd[[highlight SignColumn guibg=none]]
+	vim.api.nvim_set_hl(0, "CursorLineNr", { link = "@function.call" })
+end
