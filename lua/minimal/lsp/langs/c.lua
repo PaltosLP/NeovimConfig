2dbc0733dade67c00488ef9ca10014518b56a07d
PaltosLP <motata.frochaux@gmail.com>
2022-12-22 08:50:16 +0100

    no minimal

--- lua/minimal/lsp/langs/c.lua
+++
@@ -1,8 +0,0 @@
-local utils = require 'core.lsp.langs.utils'
-
-vim.opt.cindent = true
-
-
-
-
-return utils.on_attach
--- lua/minimal/lsp/langs/py.lua
+++
@@ -1,7 +0,0 @@
-local utils = require 'core.lsp.langs.utils'
-
-
-
-
-
-return utils.on_attach
