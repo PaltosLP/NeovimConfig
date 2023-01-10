20117b5a5ceb43627597f1a6af474ce4175fee98
PaltosLP <motata.frochaux@gmail.com>
2023-01-10 15:19:15 +0100

    ufo weird

--- lua/core/plugins/ufo.lua
+++ lua/core/plugins/ufo.lua
@@ -11,7 +11,8 @@ vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 
 require('ufo').setup({
 	open_fold_hl_timeout =  150,
-	})
+})
 
 vim.cmd.highlight("UfoFoldedFg guifg=#297508")
 vim.cmd.highlight("UfoFoldedBg guifg=#6f0e8f")
+-- vim.cmd.highlight("UfoPreviewCursorLine guifg=#6f0e8f")
