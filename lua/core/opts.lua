4e40a96dbb3e16746bb6a60f78fab803a595e644
PaltosLP <motata.frochaux@gmail.com>
2023-01-30 12:48:31 +0100

    uopa

--- lua/core/opts.lua
+++ lua/core/opts.lua
@@ -75,7 +75,7 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.o.statuscolumn = "%=%l%s%C"
+-- vim.o.statuscolumn = "%=%l%s%C"
 
 
 -- if vim.g.user_interface then
